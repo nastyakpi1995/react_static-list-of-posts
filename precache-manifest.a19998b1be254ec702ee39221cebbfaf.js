8d563c429162bbbdba99d8cf202cea9fcf9e06eb
Nastia Krat <nastyakpi1995@gmail.com>
2019-07-04 13:32:07 +0300

    Updates

---
+++ precache-manifest.a19998b1be254ec702ee39221cebbfaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a4cdf58e9694e76817369d7d433017",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "fc84103c38bd8a9467c9",
+    "url": "/react_static-list-of-posts/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react_static-list-of-posts/static/js/2.95678282.chunk.js"
+  },
+  {
+    "revision": "fc84103c38bd8a9467c9",
+    "url": "/react_static-list-of-posts/static/js/main.b6329ba2.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
